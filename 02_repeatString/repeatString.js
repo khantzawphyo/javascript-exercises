7a2d548ed65ed3a013fea9b0e6c8b2a12f8fa25e
Khant Zaw Phyo <khantzawphyo02@gmail.com>
2024-04-30 21:10:56 +0630

    Complete exercises 01_helloWorld to 07_tempConversion

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (word, times) {
+    if (times < 0) { return "Error"; }
+    let string = "";
+    for (let i = 0; i < times; i++) {
+        string += word;
+    }
+    return string;
 };
+
+console.log(repeatString("bye", 0));
 
 // Do not edit below this line
 module.exports = repeatString;
